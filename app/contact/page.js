5e5048db1b5ad492efb7c0019434b2c61b0411cf
Samit Koyom <samitkoyom@gmail.com>
2024-11-27 16:03:25 +0700

    first commit

---
+++ app/contact/page.js
@@ -0,0 +1,8 @@
+export default function Contact() {
+    return (
+        <div className="px-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <h1 className="my-6 text-4xl font-bold">Contact Page</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat pariatur, sint dolor quas distinctio facilis enim neque libero animi voluptate hic nemo ducimus officiis et atque eligendi deserunt laboriosam ipsum.</p>
+        </div>
+    )
+}
